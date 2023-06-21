096cc1a84fe37977b38687d7afcff2822aa74bd8
Rachel <gggggrea@gmail.com>
2023-06-21 22:11:53 +0100

    Add quest answer type

--- src/type.ts
+++ src/type.ts
@@ -20,3 +20,26 @@ export interface Options {
     currency: string[];
   };
 }
+
+export interface Answers {
+  preferredCuisines: string[] | undefined;
+  preferredFlavours: string[] | undefined;
+  spicyLevels: "Mild" | "Medium" | "Hot" | "Very Hot" | "Extremely";
+  preferredStyle: string[] | undefined;
+  preferredMaterials: string[] | undefined;
+  preferredIngredients: string[] | undefined;
+  dietaryRestrictions: string[] | undefined;
+  specificGoals: string[] | undefined;
+  mealSize: string;
+  mealtime: string[] | undefined;
+  cookingTime: { min: number; max: number } | undefined;
+  servingSize: { min: number; max: number };
+  cookingFacilities: string[] | undefined;
+  specificCookingTechniques: string[] | undefined;
+  specificTextures: string[] | undefined;
+  budget: {
+    unit: string;
+    value: { min: number; max: number };
+    currency: string;
+  };
+}
