ba0ab32eeb5e93fb1db6aca3459fdb9b944505c0
Rachel <gggggrea@gmail.com>
2023-07-18 18:30:16 +0100

    Create general layout of recipe results

--- frontend/src/routes/Results.tsx
+++ frontend/src/routes/Results.tsx
@@ -1,6 +1,32 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
+import {
+  List,
+  ListItem,
+  ListIcon,
+  OrderedList,
+  UnorderedList,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Heading,
+  TableContainer,
+  Thead,
+  Tr,
+  Table,
+  Tbody,
+  Td,
+  Flex,
+  VStack,
+  Th,
+  Divider,
+  Box,
+  Image,
+} from "@chakra-ui/react";
+import { Icon } from "@chakra-ui/react";
+import { TimeIcon } from "@chakra-ui/icons";
 
 import { ResRecipe } from "../type";
 
@@ -16,10 +42,75 @@ export function Results() {
   }, [formData]);
 
   return (
-    <>
-      <div> {results?.recipeName} </div>
-      <div> {results?.ingredients} </div>
-      <div> {results?.instructions} </div>
-    </>
+    <VStack paddingY={10} gap={10}>
+      <Card align="center">
+        <CardHeader>
+          <Heading size="lg"> {results?.recipeName} </Heading>
+        </CardHeader>
+        <CardBody gap={5} color="gray.500" fontWeight="semibold" letterSpacing="wide">
+          <Flex align="" gap={2}>
+            <Flex gap={1}>
+              <Image src={`https://www.countryflagicons.com/FLAT/16/${results.iso2}.png`} /> {results?.cuisineType}
+            </Flex>
+            |
+            <Flex gap={1}>
+              <TimeIcon w={5} h={5} /> {results?.totalTime} mins
+            </Flex>
+          </Flex>
+        </CardBody>
+      </Card>
+      <Flex gap={10} height="50vh">
+        <TableContainer>
+          <Table variant="simple" size="sm">
+            <Thead>
+              <Tr>
+                <Th> INFO </Th>
+                <Th> PER MEAL </Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td> Preparing Time </Td>
+                <Td isNumeric> {results?.prepTime} </Td>
+              </Tr>
+              <Tr>
+                <Td> Cooking Time </Td>
+                <Td isNumeric> {results?.cookTime} </Td>
+              </Tr>
+              <Tr>
+                <Td> Servings </Td>
+                <Td isNumeric> {results?.servings} </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </TableContainer>
+
+        <Divider orientation="vertical" />
+
+        <Flex direction="column" gap={5}>
+          <Box>
+            <Heading size="md"> Ingredients </Heading>
+            <List p={2}>
+              <UnorderedList>
+                {results?.ingredients.map((el, i) => (
+                  <ListItem key={i}>{el}</ListItem>
+                ))}
+              </UnorderedList>
+            </List>
+          </Box>
+
+          <Box>
+            <Heading size="md"> Instructions </Heading>
+            <List p={2}>
+              <OrderedList>
+                {results?.instructions.map((el, i) => (
+                  <ListItem key={i}> {el} </ListItem>
+                ))}
+              </OrderedList>
+            </List>
+          </Box>
+        </Flex>
+      </Flex>
+    </VStack>
   );
 }
