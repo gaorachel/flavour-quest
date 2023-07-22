be86eba17097089fd5dfb2e5ac88f57b3d6da8dc
Rachel <gggggrea@gmail.com>
2023-07-23 00:22:04 +0100

    Connect FE to BE

---
+++ next/flavour-quest/src/pages/api/_openai.ts
@@ -0,0 +1,24 @@
+import { Configuration, OpenAIApi } from "openai";
+
+import { Choices, Format } from "@/type";
+
+export async function askGPT(data: Choices, format: Format) {
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "user",
+        content: `Hi, can you give me 1 recipe based on the following info. No need to meet all the requirements: ${data}. . Here is the result format: ${JSON.stringify(
+          format
+        )}`,
+      },
+    ],
+  });
+
+  return JSON.parse(completion.data.choices[0].message?.content as string);
+}
---
+++ next/flavour-quest/src/pages/api/quest.ts
@@ -0,0 +1,55 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Choices, Format } from "@/type";
+import { recipeRes } from "./_mockRes";
+import axios from "axios";
+import { askGPT } from "./_openai";
+
+const imgBackup = "https://shewearsmanyhats.com/wp-content/uploads/2015/12/roasted-garlic-lemon-chicken-recipe-1.jpg";
+const resExample = {
+  recipeName: "Spaghetti Carbonara",
+  cuisineType: "Italian",
+  servings: 2,
+  prepTime: 10,
+  cookTime: 15,
+  totalTime: 25,
+  ingredients: [
+    "200g spaghetti",
+    "100g pancetta",
+    "2 cloves of garlic",
+    "2 large eggs",
+    "50g grated Parmesan cheese",
+    "Salt and black pepper to taste",
+  ],
+  instructions: [
+    "Bring a large pot of salted water to a boil and cook spaghetti according to package instructions until al dente.",
+    "Meanwhile, in a large skillet, fry pancetta until crispy. Remove from heat and set aside.",
+    "In a separate bowl, whisk together eggs, grated Parmesan cheese, salt, and black pepper.",
+    "Once spaghetti is cooked, drain it and quickly toss it in the skillet with the pancetta. Remove from heat.",
+    "Pour the egg mixture over the hot spaghetti while stirring continuously, allowing the heat to cook the eggs and create a creamy sauce.",
+    "Serve immediately and garnish with additional grated Parmesan cheese and black pepper if desired.",
+  ],
+};
+
+export default async function questHandler(req: NextApiRequest, res: NextApiResponse<any>) {
+  try {
+    const formData: Choices = req.body;
+    const recipeRes: Format = await askGPT(formData, resExample);
+    const imgRes = await axios.get("https://www.googleapis.com/customsearch/v1", {
+      params: {
+        key: process.env.GOOGLE_SEARCH_API_KEY,
+        cx: process.env.GOOGLE_SEARCH_API_CX,
+        searchType: "image",
+        num: 1,
+        hq: "16:9",
+        imgSize: "large",
+        gl: "countryUK",
+        q: recipeRes.recipeName,
+      },
+    });
+    res.status(201).send({ ...recipeRes, img: imgRes.data.items[0].link || imgBackup });
+
+    // res.send({ ...recipeRes, img: imgBackup });
+  } catch (e) {
+    res.status(500).send(e);
+  }
+}
