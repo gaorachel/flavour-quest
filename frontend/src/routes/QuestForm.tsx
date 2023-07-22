c625837841b60d2011900d739b23da8b1eaff2a2
Rachel <gggggrea@gmail.com>
2023-07-22 12:27:35 +0100

    Fix bug
    Fix bug

--- frontend/src/routes/QuestForm.tsx
+++ frontend/src/routes/QuestForm.tsx
@@ -53,6 +53,7 @@ export function QuestFrom() {
       if (key === "cookingTime") valueText = `${Object.values(value)[0]} - ${Object.values(value)[1]} mins`;
       if (key === "servingSize") valueText = `${Object.values(value)[0]} - ${Object.values(value)[1]} people`;
       if (key === "budget") valueText = `${value.currency} ${value.value.min} - ${value.value.max} ${value.unit}`;
+      if (key === "spicyLevels") valueText = value;
 
       if (value.length != 0) sortedAnswers.push(`${sortedKeyText}: ${valueText}`);
     }
@@ -61,32 +62,35 @@ export function QuestFrom() {
 
   const sendAPIReq = async (answers: Choices | Choices) => {
     const res = await axios.post("/api/v1/quest", answers);
-    navigate("/results", { state: res.data });
-    // setTimeout(() => {
-    //   navigate("/results", { state: res.data });
-    // }, 50000);
+    // navigate("/results", { state: res.data });
+    setTimeout(() => {
+      navigate("/results", { state: res.data });
+    }, 50000);
   };
 
   const handleSubmit = async (answers: Choices) => {
     displayAnswersOnModal(answers), sendAPIReq(answers);
   };
 
-  const randomNumBasedOnInput = (data: string[] | object) => {
+  const randIntBy = (data: string[] | object) => {
     let len = 0;
     if (typeof data === "object") len = Object.keys(data).length;
     if (Array.isArray(data)) len = data.length;
 
-    return Math.max(Math.floor(Math.random() * len), 5); // displaying more than 6 options looks bad
+    return Math.floor(Math.random() * len);
+  };
+
+  const randIntBetween = (min: number, max: number) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
   };
 
   const handleClick = (options: Choices) => {
     onOpen(); // show modal on the screen
 
-    const ranSelectTimes = randomNumBasedOnInput(options) + 2; // + 2 is to select more questions
     let pickedOptions: Choices = {};
-    for (let time = 0; time < ranSelectTimes; ) {
-      const ranNum = randomNumBasedOnInput(options);
-      const pickedEl = Object.fromEntries([Object.entries(options)[ranNum]]);
+    for (let time = 0; time < randIntBetween(3, 9); ) {
+      const randIdx = randIntBy(options);
+      const pickedEl = Object.fromEntries([Object.entries(options)[randIdx]]);
 
       pickedOptions = { ...pickedOptions, ...pickedEl };
       time++;
@@ -94,43 +98,51 @@ export function QuestFrom() {
 
     let pickedAnswers: Choices = {};
     for (const [key, value] of Object.entries(pickedOptions)) {
-      if (Array.isArray(value)) {
-        const ranNum = randomNumBasedOnInput(value);
-        const pickedValues = new Set();
-        for (let time = 0; time < ranNum; ) {
-          if (value[ranNum] !== "Any") pickedValues.add(value[ranNum]);
+      if (key === "spicyLevels") {
+        const randIdx = randIntBetween(0, 4);
 
-          pickedAnswers = { [key]: [...pickedValues] };
+        pickedAnswers = { ...pickedAnswers, [key]: value[randIdx] };
+      }
+
+      if (Array.isArray(value)) {
+        const pickedValues = new Set();
+        for (let time = 0; time < randIntBetween(1, 7); ) {
+          const randIdx = randIntBy(value);
+          if (value[randIdx] !== "Any") pickedValues.add(value[randIdx]);
+
+          pickedAnswers = { ...pickedAnswers, [key]: [...pickedValues] };
           time++;
         }
       }
 
       if (key === "servingSize" || key === "cookingTime") {
-        const ranNum = Math.random() * 10;
+        const ranNum = randIntBetween(1, 5);
         const min = 1 * ranNum;
-        const max = 10 * ranNum;
+        const max = 2 * ranNum;
 
-        pickedAnswers = { [key]: { min, max } };
+        pickedAnswers = { ...pickedAnswers, [key]: { min, max } };
       }
 
       if (key === "budget") {
-        const ranNum = Math.random() * 10;
-        const unit = value.unit[Math.round(Math.random())];
-        const min = value.value.min * ranNum;
-        const max = value.value.max * ranNum;
-        const currency = value.currency[Math.floor(Math.random() * 3)];
+        const unit = value.unit[randIntBetween(0, 1)];
+        const min = value.value.min * randIntBetween(1, 3);
+        const max = value.value.max * randIntBetween(1, 3);
+        const currency = value.currency[randIntBetween(0, 2)];
 
-        pickedAnswers[key] = {
-          unit,
-          value: {
-            min,
-            max,
+        pickedAnswers = {
+          ...pickedAnswers,
+          [key]: {
+            unit,
+            value: {
+              min,
+              max,
+            },
+            currency,
           },
-          currency,
         };
       }
     }
-    handleSubmit(pickedOptions);
+    handleSubmit(pickedAnswers);
   };
 
   return (
@@ -181,15 +193,15 @@ export function QuestFrom() {
 
                 <>
                   <FormLabel> How spicy would you like? </FormLabel>
-                  <Center>
-                    <Field
-                      name="spicyLevels"
-                      component={RadioGroup}
-                      optionArr={options.spicyLevels}
-                      defaultValue={options.spicyLevels[2]}
-                      borderRadius="full"
-                    />
-                  </Center>
+                  {/* <Center> */}
+                  <Field
+                    name="spicyLevels"
+                    component={RadioGroup}
+                    optionArr={options.spicyLevels}
+                    defaultValue={options.spicyLevels[2]}
+                    borderRadius="full"
+                  />
+                  {/* </Center> */}
                 </>
 
                 <Field
@@ -231,8 +243,8 @@ export function QuestFrom() {
                   name="servingSize"
                   formLabel="How many people will you be cooking for?"
                   component={NumInput}
-                  min={options.servingSize.min}
-                  max={options.servingSize.max}
+                  min={options.servingSize?.min}
+                  max={options.servingSize?.max}
                   defaultValue={[1, 3]}
                 />
 
@@ -247,7 +259,7 @@ export function QuestFrom() {
                   name="cookingTime"
                   formLabel="How much time (minutes) would you prefer to spend on cooking/preparing the meal?"
                   component={RangeSliderWithIndexValue}
-                  defaultValue={[options.cookingTime.min, options.cookingTime.max]}
+                  defaultValue={[options.cookingTime?.min, options.cookingTime?.max]}
                   min={0}
                   max={120}
                 />
@@ -280,23 +292,23 @@ export function QuestFrom() {
                     <Field
                       name="budget.currency"
                       component={RadioGroup}
-                      optionArr={options.budget.currency}
-                      defaultValue={options.budget.currency[0]}
+                      optionArr={options.budget?.currency}
+                      defaultValue={"GBP"}
                       borderRadius="full"
                     />
 
                     <Field
                       name="budget.unit"
                       component={RadioGroup}
-                      optionArr={options.budget.unit}
-                      defaultValue={options.budget.unit[0]}
+                      optionArr={options.budget?.unit}
+                      defaultValue={"per person"}
                       space={0}
                     />
                   </HStack>
                   <Field
                     name="budget.value"
                     component={RangeSliderWithIndexValue}
-                    defaultValue={[options.budget.value.min, options.budget.value.max]}
+                    defaultValue={[options.budget?.value?.min, options?.budget?.value?.max]}
                     min={0}
                     max={200}
                   />
