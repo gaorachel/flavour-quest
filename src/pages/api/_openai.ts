c2f3748f03122155d58c38599e0a977b6790eb63
Onur Ozcan <onur@9fin.com>
2023-09-16 14:36:42 +0100

    Do it

--- src/pages/api/_openai.ts
+++ src/pages/api/_openai.ts
@@ -1,18 +1,22 @@
-import { Configuration, OpenAIApi } from "openai";
+import { Configuration, OpenAIApi } from 'openai';
 
-import type { Choices, Format } from "@/type";
+import type { Choices, Format } from '@/type';
 
-export async function askGPT(data: Choices, format: Format) {
+async function timer(seconds: number) {
+  await new Promise((_resolve, reject: any) => setTimeout(() => reject('took too long'), seconds));
+}
+
+async function longJob(data: Choices, format: Format) {
   const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
   });
   const openai = new OpenAIApi(configuration);
 
   const completion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
+    model: 'gpt-3.5-turbo',
     messages: [
       {
-        role: "user",
+        role: 'user',
         content: `Hi, can you give me 1 recipe based on the following info. No need to meet all the requirements: ${data}. . Here is the result format: ${JSON.stringify(
           format
         )}`,
@@ -21,4 +25,8 @@ export async function askGPT(data: Choices, format: Format) {
   });
 
   return JSON.parse(completion.data.choices[0].message?.content as string);
+}
+
+export function askGPT(data: Choices, format: Format) {
+  return Promise.race([longJob(data, format), timer(10000)]);
 }
--- src/pages/api/quest.ts
+++ src/pages/api/quest.ts
@@ -1,53 +1,53 @@
-import { mockRes } from "./_mockRes";
-import axios from "axios";
-import { askGPT } from "./_openai";
+import { mockRes } from './_mockRes';
+import axios from 'axios';
+import { askGPT } from './_openai';
 
-import type { NextApiRequest, NextApiResponse } from "next";
-import type { Choices, Format } from "@/type";
+import type { NextApiRequest, NextApiResponse } from 'next';
+import type { Choices, Format } from '@/type';
 
 const imgBackup =
-  "https://assets.bonappetit.com/photos/57ae3e611b33404414975c0d/16:9/w_1280,c_limit/roasted-chicken-thighs-with-lemon-and-oregano.jpg";
+  'https://assets.bonappetit.com/photos/57ae3e611b33404414975c0d/16:9/w_1280,c_limit/roasted-chicken-thighs-with-lemon-and-oregano.jpg';
 const resExample = {
-  recipeName: "Spaghetti Carbonara",
-  cuisineType: "Italian",
+  recipeName: 'Spaghetti Carbonara',
+  cuisineType: 'Italian',
   servings: 2,
   prepTime: 10,
   cookTime: 15,
   totalTime: 25,
   ingredients: [
-    "200g spaghetti",
-    "100g pancetta",
-    "2 cloves of garlic",
-    "2 large eggs",
-    "50g grated Parmesan cheese",
-    "Salt and black pepper to taste",
+    '200g spaghetti',
+    '100g pancetta',
+    '2 cloves of garlic',
+    '2 large eggs',
+    '50g grated Parmesan cheese',
+    'Salt and black pepper to taste',
   ],
   instructions: [
-    "Bring a large pot of salted water to a boil and cook spaghetti according to package instructions until al dente.",
-    "Meanwhile, in a large skillet, fry pancetta until crispy. Remove from heat and set aside.",
-    "In a separate bowl, whisk together eggs, grated Parmesan cheese, salt, and black pepper.",
-    "Once spaghetti is cooked, drain it and quickly toss it in the skillet with the pancetta. Remove from heat.",
-    "Pour the egg mixture over the hot spaghetti while stirring continuously, allowing the heat to cook the eggs and create a creamy sauce.",
-    "Serve immediately and garnish with additional grated Parmesan cheese and black pepper if desired.",
+    'Bring a large pot of salted water to a boil and cook spaghetti according to package instructions until al dente.',
+    'Meanwhile, in a large skillet, fry pancetta until crispy. Remove from heat and set aside.',
+    'In a separate bowl, whisk together eggs, grated Parmesan cheese, salt, and black pepper.',
+    'Once spaghetti is cooked, drain it and quickly toss it in the skillet with the pancetta. Remove from heat.',
+    'Pour the egg mixture over the hot spaghetti while stirring continuously, allowing the heat to cook the eggs and create a creamy sauce.',
+    'Serve immediately and garnish with additional grated Parmesan cheese and black pepper if desired.',
   ],
 };
 
 export default async function questHandler(req: NextApiRequest, res: NextApiResponse<any>) {
-  if (process.env.NODE_ENV === "development") return res.send({ ...mockRes, img: imgBackup });
+  if (process.env.NODE_ENV === 'development') return res.send({ ...mockRes, img: imgBackup });
 
   try {
     const formData: Choices = req.body;
     const recipeRes: Format = await askGPT(formData, resExample);
 
     try {
-      const imgRes = await axios.get("https://www.googleapis.com/customsearch/v1", {
+      const imgRes = await axios.get('https://www.googleapis.com/customsearch/v1', {
         params: {
           key: process.env.GOOGLE_SEARCH_API_KEY,
           cx: process.env.GOOGLE_SEARCH_API_CX,
-          searchType: "image",
+          searchType: 'image',
           num: 1,
-          hq: "16:9",
-          gl: "countryUK",
+          hq: '16:9',
+          gl: 'countryUK',
           q: recipeRes.recipeName,
         },
       });
