fda77fe6fa23d0ddbc01cb0e30d2a00948858562
Rachel <gggggrea@gmail.com>
2023-09-16 14:45:01 +0100

    Fix formatting

--- src/pages/api/_openai.ts
+++ src/pages/api/_openai.ts
@@ -1,9 +1,9 @@
-import { Configuration, OpenAIApi } from 'openai';
+import { Configuration, OpenAIApi } from "openai";
 
-import type { Choices, Format } from '@/type';
+import type { Choices, Format } from "@/type";
 
 async function timer(seconds: number) {
-  await new Promise((_resolve, reject: any) => setTimeout(() => reject('took too long'), seconds));
+  await new Promise((_resolve, reject: any) => setTimeout(() => reject("took too long"), seconds));
 }
 
 async function longJob(data: Choices, format: Format) {
@@ -13,10 +13,10 @@ async function longJob(data: Choices, format: Format) {
   const openai = new OpenAIApi(configuration);
 
   const completion = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
+    model: "gpt-3.5-turbo",
     messages: [
       {
-        role: 'user',
+        role: "user",
         content: `Hi, can you give me 1 recipe based on the following info. No need to meet all the requirements: ${data}. . Here is the result format: ${JSON.stringify(
           format
         )}`,
