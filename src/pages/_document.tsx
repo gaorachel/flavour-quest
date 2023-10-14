30be3e314c2da13a57e9fc4c49786f9ed22c1a14
Rachel <gggggrea@gmail.com>
2023-10-14 21:48:43 +0100

    Fix bug

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="shortcut icon" href="/static/favicon.png" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
         <Main />
